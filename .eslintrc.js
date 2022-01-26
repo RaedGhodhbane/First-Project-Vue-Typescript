9b793518eba6a76fdfa5c9fa6d284fdaecb98b74
Adrien <adrien.mollat@orange.fr>
2022-01-26 12:20:57 +0100

    First commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    },
+    "extends": "eslint:recommended"
 }
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="app">
+  <p> {{jobs[0].location}}</p>
+
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job'
+
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name : 'Link',
+    //   age: 25 as string | number
+
+    // })
+
+    // state.name = 'Shaun' 
+    // state.age = '26'
+
+    // return { ...toRefs(state)}
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return { name, age}
+
+  const jobs = ref<Job[]>([
+    { title: 'farm worker',location:'lon lon ranch', salary: 30000, id:'1'},
+    { title: 'farm worker',location:'lon lon ranch', salary: 30000, id:'2'}
+  ])
+    return {jobs}
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+
