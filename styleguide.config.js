6958d3e70aaa0ec82e24d71f74f9b60ad786b0b6
RaedGhodhbane <ghodhbane.raed@gmail.com>
2022-01-26 18:06:21 +0100

    Feat : Styleguidist installation

---
+++ styleguide.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // set your styleguidist configuration here
+  title: "Default Style Guide",
+  // components: 'src/components/**/[A-Z]*.vue',
+  // defaultExample: true,
+  // sections: [
+  //   {
+  //     name: 'First Section',
+  //     components: 'src/components/**/[A-Z]*.vue'
+  //   }
+  // ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // },
+  exampleMode: "expand",
+  scripts: {
+    styleguide: "vue-styleguidist server",
+    "styleguide:build": "vue-styleguidist build",
+  },
+};
